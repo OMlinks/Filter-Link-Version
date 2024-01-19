7d78a331b2788dd23d38293ea9a1f167cabac30d
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-01-19 22:07:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -59,7 +59,7 @@ async def start(client, message):
         await m.delete()         
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_MESSAGE.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=START_MESSAGE.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -111,7 +111,7 @@ async def start(client, message):
         await m.delete()         
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_MESSAGE.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=START_MESSAGE.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
