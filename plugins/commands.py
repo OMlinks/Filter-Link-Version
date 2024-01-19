0f8a8d92d6fcfc22a47c1b9335a56ab28623d05e
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-01-19 18:23:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,6 +50,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton("🎭 ɢʀᴏᴜᴩ 🎭", url="https://t.me/+rxBzvfTcvnIwYmM1")
         ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
         m = await message.reply_sticker("CAACAgUAAxkBAAJiVGVi67X3-k_Jmpl_Vn8LAsCEee49AALwBAAC0zvRV6GPpPtRmRhrMwQ") 
         await asyncio.sleep(3)
         await message.reply_photo(photo=random.choice(PICS), caption=script.START_TXT.format(user=message.from_user.mention, bot=client.mention), reply_markup=InlineKeyboardMarkup(buttons), parse_mode=enums.ParseMode.HTML)
