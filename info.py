9d68f1c2349e3ce6fa598de3c5c597cae0bfe148
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-01-19 15:28:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,8 +22,8 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', '')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/4f2630c67bf86601c7035.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/ad2bf871f05138e234182.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
