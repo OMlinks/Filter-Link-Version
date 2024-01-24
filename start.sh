7eb7e2246344b1d1fbf7a39ea8ac728ede95c6d6
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-01-25 01:15:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/OMlinks/Filter-Link-Version.git /Auto-search-tamil-bot
+  git clone https://github.com/OMlinks/Filter-Link-Version.git /Filter-Link-Version
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Filter-Link-Version
