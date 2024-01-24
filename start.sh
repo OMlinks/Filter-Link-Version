873c29ed58db079d1b1d47761795a9686a8923cc
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-01-25 01:15:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/OMlinks/Filter-Link-Version.git /Auto-search-tamil-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /Filter-Link-Version
 fi
-cd /Auto-search-tamil-bot
+cd /Filter-Link-Version
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
